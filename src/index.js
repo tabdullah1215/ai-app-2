d0e51bc5c4b6caa9fe42dac97dbb1bf1c42580e7
Tariq Abdullah <tabdullah1215@gmail.com>
2024-02-04 00:46:22 -0500

    3rd commit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.scss';
 
-const App = () => <h1>Hello World!!</h1>;
+const App = () => <>
+    <h1>Hello World!!</h1>
+    <div>Amplify is the Best!</div>
+    </>;
 
 ReactDOM.render(<App />, document.getElementById('app'));
