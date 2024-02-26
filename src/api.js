9f52b548ae688adfcb42b1b6332c750003907d2c
Tariq Abdullah <tabdullah1215@gmail.com>
2024-02-25 22:12:57 -0500

    remove comments

--- src/api.js
+++ src/api.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
-// const REACT_APP_AI_API_KEY_1 = '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';
 export const fetchChatGPTResponse = async (userInput) => {
     try {
         const response = await axios.post(
@@ -17,7 +16,7 @@ export const fetchChatGPTResponse = async (userInput) => {
             },
             {
                 headers: {
-                    'Authorization': `Bearer ${REACT_APP_AI_API_KEY_1}`,
+                    'Authorization': `Bearer ${process.env.AI_API_KEY_1}`,
                     'Content-Type': 'application/json',
                 },
             }
--- src/index.js
+++ src/index.js
@@ -3,9 +3,4 @@ import ReactDOM from 'react-dom';
 import './style.scss';
 import App from './App';
 
-// const App = () => <>
-//     <h1>Hello World!!</h1>
-//     <div>Amplify is the Best!!!***</div>
-//     </>;
-
 ReactDOM.render(<App />, document.getElementById('app'));
