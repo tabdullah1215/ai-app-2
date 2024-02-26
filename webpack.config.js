df8ccccec8ec65631e72363ecfe220a9c6d22b75
Tariq Abdullah <tabdullah1215@gmail.com>
2024-02-25 22:24:26 -0500

    add configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
     entry: './src/index.js',
@@ -33,7 +34,7 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: './src/index.html'
-        })
+        }), new Dotenv() // handles .env file
     ],
     devServer: {
         static: {
