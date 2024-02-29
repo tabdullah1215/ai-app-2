7b6712eca8701badd8436ef0a401272860fee80f
Tariq Abdullah <tabdullah1215@gmail.com>
2024-02-28 23:43:36 -0500

    add amplify init dependencies

--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.scss';
 import App from './App';
+import Amplify from 'aws-amplify';
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
+
 
 ReactDOM.render(<App />, document.getElementById('app'));
